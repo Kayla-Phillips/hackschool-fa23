b96b3ca86a40fbbf760a6c19d479dabbea5e7003
Kayla-Phillips <k3phillips@ucsd.edu>
2023-11-01 17:47:02 -0700

    week 3 workshop

--- client/src/components/navbar-component/navbar.jsx
+++ client/src/components/navbar-component/navbar.jsx
@@ -1,18 +1,27 @@
-import styles from './Navbar.module.css'
+import styles from './navbar.module.css'
+import Link from 'next/link'
 
 const Navbar = () => {
     return (
         /* return a navbar with a top level div, and links to the homepage, Game page, and History page! */
         /* Hint: the Next.js Link component may come in handy https://nextjs.org/docs/pages/api-reference/components/link */
-        <div id={styles.navbar}>
+        <div id={styles["navbar"]}>
             <div className={styles.title}>
                 {/* Link to the home page here! */}
+                <Link href = "/">HackRacer</Link>
             </div>
 
             {/* Link to other pages here! */}
+            <div className={styles.nav_buttons}> 
+                <Link href = "/addSentence">Add Sentence</Link>          
+                <Link href = "/game">Play Game</Link>
+                <Link href = "/history">History</Link>
+            </div>
+ 
 
         </div>
     )
 }
 
 // Export the NavBar, and import it in pages/_app.jsx to render it on all pages of your website!
+export default Navbar;
--- client/src/pages/index.jsx
+++ client/src/pages/index.jsx
@@ -1,5 +1,6 @@
 import Image from 'next/image';
 import styles from '../styles/Home.module.css'
+import acmLogo from '../assets/acmlogo.png'
 
 
 export default function Home() {
@@ -7,12 +8,14 @@ export default function Home() {
     /* Each function in JSX can only return ONE DOM element, so we have one top-level div */
     <div className={styles.main}>
         <div className={styles.welcome}>
-          <h1> Hello World! </h1>
-          {/* Replace the heading, and add a short paragraph about your website here! */}
+          <h2> Welcome to HackRacer! </h2>
+          <p>You can navigate to <i>Add Sectence</i> button at the top to add a new sentence. Then you can go over to <i>Play Game</i> to start playing the game.</p>
+            {/* Replace the heading, and add a short paragraph about your website here! */}
         </div>
       <div className={styles.image}>
         {/* Put the ACM logo here! */}
         {/* Keep in mind that any images you want to use must be imported from the /assets folder */}
+        <Image src={acmLogo} width = {200} height = {200} alt = "ACM Logo"/>
       </div>
     </div>
   )
